cb3b0ea1694ad88f489207d6f38e77e774d506dc
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-04 23:38:29 +0600

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ function Navbar() {
         <nav className="">
             {/* one */}
             <a href="" className="one">হোম</a>
-            <a href="" className="one">বাংলাদেশ সম্পর্কিত</a>
-            <a href="" className="one">ই-সেবাসমুহ</a>
-            <a href="" className="one"></a>
-            <a href="" className="one">হোম</a>
-            <a href="" className="one">হোম</a>
-            <a href="" className="one">হোম</a>
-            <a href="" className="one">হোম</a>
-            <a href="" className="one">হোম</a>
-            <a href="" className="one">হোম</a>
+            <a href="" className="two">বাংলাদেশ সম্পর্কিত</a>
+            <a href="" className="three">ই-সেবাসমুহ</a>
+            <a href="" className="four">সেবাখাত</a>
+            <a href="" className="five">ব্যবসা-বাণিজ্য</a>
+            <a href="" className="six">বৈদেশিক বিনিয়োগ</a>
+            <a href="" className="seven">আইন-বিধি</a>
+            <a href="" className="eight">তথ্য বাতায়ন</a>
+            <a href="" className="nine">সেবাকুঞ্জ</a>
+            <a href="" className="ten">ফরমস</a>
 
              
         </nav>
